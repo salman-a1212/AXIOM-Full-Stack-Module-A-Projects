419f72c0c6d3945dd6859953d4da667e0a6860b9
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2021-05-05 11:38:40 +0500

    Update script.js
    created script.js by archiving old JS code for refactoring

--- Project-1/script.js
+++ Project-1/script.js
@@ -1,62 +1,76 @@
-// Retrieving HTML elements from the DOM
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-// Function to update class and message for errors
-function showError(input, message) {
-    // Get the parent element of the input field (.form-control)
+// All Functions
+// Function to show error
+function showError(input,message) {
     const formControl = input.parentElement;
-    // Replace the class - add error
     formControl.className = 'form-control error';
-    // Get the small element for the error message
     const small = formControl.querySelector('small');
-    // Replace the text for small element using the input message
     small.innerText = message;
 }
 
-// Function to update class for success
+// Function to show success
 function showSuccess(input) {
-    // Get the parent element of the input field (.form-control)
     const formControl = input.parentElement;
-    // Replace the class - add success
     formControl.className = 'form-control success';
 }
 
-// Event Listeners
-// Create event listener for submit button
-form.addEventListener('submit', function(e) {
-    // Stop page from reloading on submit
+// Function to check if email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if ( re.test(input.value.trim()) ) {
+        showSuccess(input);
+    } else {
+        showError(input, `Please provide a valid email`)
+    }
+}
+
+// Function to check if required fields have data
+function checkRequired(inputArray) {
+    inputArray.forEach(function(input) {
+        if ( input.value === '' ) {
+            showError(input,`${getFieldId(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+// Function to check length of input field
+function checkLength(input, min, max) {
+    if ( input.value.length < min ) {
+        showError(input,`${getFieldId(input)} needs to be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input,`${getFieldId(input)} needs to be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Function to check if password and confirm password match
+function checkPasswordsMatch(input1, input2) {
+    if ( input1.value !== input2.value ) {
+        showError(input2,"Passwords don't match")
+    }
+}
+
+// Function to get the id of the input field with proper case
+function getFieldId(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// This is an event listener for the form on submit
+form.addEventListener('submit',function(e) {
     e.preventDefault();
 
-    // Check to see if fields meet required field requirement
-    // Check if username input is empty
-    if(username.value === '') {
-        showError(username, 'Username is required');
-    } else {
-        showSuccess(username);
-    }
+    checkRequired([username,email,password,password2]);
+    checkLength(username,3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
+    checkPasswordsMatch(password,password2);
 
-    // Check if email input is empty
-    if(email.value === '') {
-        showError(email, 'Email is required');
-    } else {
-        showSuccess(email);
-    }
-
-    // Check if password input is empty
-    if(password.value === '') {
-        showError(password, 'Password is required');
-    } else {
-        showSuccess(password);
-    }
-
-    // Check if password2 input is empty
-    if(password2.value === '') {
-        showError(password2, 'Confirm Password is required');
-    } else {
-        showSuccess(password2);
-    }
-});
+})
