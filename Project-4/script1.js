f3b122d2b5b34a7bc895264a1065a07b27fc3a43
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2021-06-27 11:20:12 +0500

    Update script1.js

--- Project-4/script1.js
+++ Project-4/script1.js
@@ -1,56 +1,55 @@
-// Get DOM Elements
-const app2CurrencyOne = document.getElementById('app2_currency_one');
+// Get the DOM Elements
+const app2Currency = document.getElementById('app2_currency_one');
 const app2Amount = document.getElementById('app2_amount');
-const app2Rate = document.getElementById('app2-rate');
-
-// Fetch Exchange Rates & Update the DOM
-function calculate() {
-    // Get the Currency Code for input currency
-    const app2CurrencyCode = app2CurrencyOne.value;
-    // console.log(app2CurrencyCode);
-    // Send Request to ExchangeRate-API for conversion rates for currency one
-    fetch(`https://v6.exchangerate-api.com/v6/410368c0d4ae2659ea293d58/latest/${app2CurrencyCode}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log('data ', data?.conversion_rates);
-            // Get the Conversion Rate from input currency to all currency list
-            let dataFetched = data.conversion_rates;
-            const app2ConverRate = data.conversion_rates;
-            console.log(app2ConverRate);
-            const keys = Object.keys(app2ConverRate);
-
-            addElement(app2ConverRate);
-            // //clear previous data  // 
-            table.innerHTML = `<h4>Currency <span>Amount</span></h4>`;
-        })
-    function addElement(app2_currency_one) {
-
-        for (keys in app2_currency_one) {
-            console.log(keys + ":" + app2_currency_one[keys])
-            const element = document.createElement('table');
-            element.classList.add('element'); //For CSS style
-            const newContent = document.createTextNode(`${keys}: ${format2Currency((parseFloat(rate[keys])) * app2CurrencyOne.value)}`);
-
-            element.appendChild(newContent);
-            const currentDiv = document.getElementById("div1");
-            document.body.insertBefore(element, currentDiv);
-
-        }
-    }
-}
-
-
-
-
-// / Event Listeners
-// Recalculate exchange rate when currency 1 changes
-app2CurrencyOne.addEventListener('change', calculate);
-// Recalculate exchange amount when currency 1 amount changes
-app2Amount.addEventListener('input', calculate);
+const exchangeRates = document.getElementById('exchange-rates');
+const paste = document.getElementById('paste');
+const ratesTable = document.getElementById("exchange-rates");
 
 // Function to format currency amount
 function format2Currency(number) {
     return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&, ');
 }
 
-calculate();
+
+
+// Send request to ExchangeRate-API for conversion rates for the base currency
+async function getExchangeRates() {
+    // Get the currency code for the base currency
+    const app2CurrencyCode = app2Currency.value;
+    console.log(app2CurrencyCode);
+    // wait for the result from API
+    const res = await fetch(`https://v6.exchangerate-api.com/v6/ff5446d657010e0e02233999/latest/${app2CurrencyCode}`);
+    // Wait for response to convert into JSON
+    const data = await res.json();
+
+    // console.log(data);
+
+    // Get the conversion rates data
+    const rates = data.conversion_rates;
+    const keys = Object.keys(rates);
+
+    //clear previous data 
+    ratesTable.innerHTML = `<h4>Currency <span>Amount</span></h4>`;
+    // 
+    keys.forEach((key) => {
+        // Create a new div element for each currency exchange rate
+        const Div = document.createElement('div');
+        // Apply the user class to the new div
+        Div.classList.add('data');
+        // Add inner HTML to the Div
+        Div.innerHTML = `${key} <span>${format2Currency((parseFloat(rates[key]))*app2Amount.value)}</span>`
+        // Add the new div into the DOM
+        ratesTable.appendChild(Div);
+    });
+}
+
+        
+             
+                
+
+
+//Event Listener
+
+app2Currency.addEventListener("change", getExchangeRates);
+
+app2Amount.addEventListener("input", getExchangeRates);
