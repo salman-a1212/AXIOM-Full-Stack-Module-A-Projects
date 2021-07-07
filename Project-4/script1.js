25e0549aa58c8d76e140e7297c3243db073846e2
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2021-07-07 11:03:23 +0500

    Update script1.js

--- Project-4/script1.js
+++ Project-4/script1.js
@@ -43,13 +43,82 @@ async function getExchangeRates() {
         // Apply the user class to the new div
         Div.classList.add('data');
         // Add inner HTML to the Div
-        Div.innerHTML = `${key} <span>${format2Currency((parseFloat(rates[key])) * app2Amount.value)}</span>`
+        Div.innerHTML = `${total} ${key} <span>${format2Currency((parseFloat(rates[key])) * app2Amount.value)}</span>`
         // Add the new div into the DOM
         ratesTable.appendChild(Div);
     });
 }
 
+// tyring out sth
 
+var initElement = document.getElementsByTagName("html")[0];
+var json = mapDOM(initElement, true);
+console.log(json);
+
+initElement = "<div><span>text</span>Text2</div>";
+json = mapDOM(initElement, true);
+console.log(json);
+
+function mapDOM(element, json) {
+    var treeObject = {};
+    
+    // If string convert to document Node
+    if (typeof element === "string") {
+        if (window.DOMParser)
+        {
+              parser = new DOMParser();
+              docNode = parser.parseFromString(element,"text/xml");
+        }
+        else // Microsoft strikes again
+        {
+              docNode = new ActiveXObject("Microsoft.XMLDOM");
+              docNode.async = false;
+              docNode.loadXML(element); 
+        } 
+        element = docNode.firstChild;
+    }
+    
+    //Recursively loop through DOM elements and assign properties to object
+    function treeHTML(element, object) {
+        object["type"] = element.nodeName;
+        var nodeList = element.childNodes;
+        if (nodeList != null) {
+            if (nodeList.length) {
+                object["content"] = [];
+                for (var i = 0; i < nodeList.length; i++) {
+                    if (nodeList[i].nodeType == 3) {
+                        object["content"].push(nodeList[i].nodeValue);
+                    } else {
+                        object["content"].push({});
+                        treeHTML(nodeList[i], object["content"][object["content"].length -1]);
+                    }
+                }
+            }
+        }
+        if (element.attributes != null) {
+            if (element.attributes.length) {
+                object["attributes"] = {};
+                for (var i = 0; i < element.attributes.length; i++) {
+                    object["attributes"][element.attributes[i].nodeName] = element.attributes[i].nodeValue;
+                }
+            }
+        }
+    }
+    treeHTML(element, treeObject);
+    
+    return (json) ? JSON.stringify(treeObject) : treeObject;
+}
+
+// iterate through elements
+var list = app2Currency,
+    option = list.getElementsByTagName("div"),
+    total = 0;
+
+for (var i = 0, len = option.length; i < len; i++) {
+    total += +option[i].value;
+}
+
+console.log(total);
 
 
 
